aec9b1d0a06a41facf11652516c6fa3f3d626e8a
HZhalex <97648418+HZhalex@users.noreply.github.com>
2024-12-05 20:57:10 +0700

    Add files via upload

---
+++ laff_dot.m
@@ -0,0 +1,38 @@
+function [alpha] = laff_dot(x,y)
+[m_x,n_x] = size(x);
+[m_y,n_y] = size(y);
+
+if (m_x ~= 1 && n_x ~= 1) | (m_y ~= 1 && n_y ~= 1)
+    alpha = 'FAILED';
+    return
+end
+
+if (m_x * n_x ~= m_y * n_y)
+    alpha = 'FAILED';
+    return
+end
+alpha = 0
+if (m_x == 1 )
+    if (m_y == 1 ) 
+        for i = 1:n_x
+            alpha = alpha + x(1,i) * y(1,i);
+        end
+    else
+        for i = 1:n_x
+            alpha = alpha + x(1,i)*y(i,1);
+        end
+    end
+else 
+    if (m_y == 1 ) 
+        for i = 1:m_x
+            alpha = alpha + x(i,1)*y(1,i);
+        end
+    else
+        for i = 1:m_x
+            alpha = alpha + x(i,1)*y(i,1);
+        end
+    end
+end
+
+return
+end
